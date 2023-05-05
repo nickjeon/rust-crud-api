17e57707688403edc17420277a11b164bd294b52
nickjeon <nicholaskjeon@gmail.com>
2023-05-05 01:02:05 -0400

    setup db connection

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,59 @@ use std::env;
 
 #[macro_use]
 extern crate serde_derive;
+
+// Model: User struct with id, name, email
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: Option<i32>,
+    name: String,
+    email: String,
+}
+
+//DATABASE URL
+const DB_URL: &str = env!("DATABASE_URL");
+
+//cosntants
+const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
+
+
+// main function
+fn main() {
+    // Set Database
+    if let Err(_) = set_database() {
+        println!("Error setting database");
+        return;
+    }
+
+    // start server and print port
+    let listener = TcpListener::bind(format!("0.0.0.0:8080")).unwrap();
+    println!("Server listening on port 8080");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                println!("Unable to connect: {}", e);
+            }
+        }
+    }
+}
+
+// db setup
+fn set_database() -> Result<(), PostgresError> {
+    let mut client = Client::connect(DB_URL, NoTls)?;
+    client.batch_execute(
+        "
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR NOT NULL,
+            email VARCHAR NOT NULL
+        )
+    "
+    )?;
+    Ok(())
+}
